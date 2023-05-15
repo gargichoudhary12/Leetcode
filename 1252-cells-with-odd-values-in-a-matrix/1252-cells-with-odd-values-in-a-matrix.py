9083e9ea316af2ee270756d4362887e5fcacd35e
gargichoudhary12 <104214078+gargichoudhary12@users.noreply.github.com>
2023-05-15 22:59:52 +0530

    Time: 176 ms (5.14%), Space: 33.5 MB (8.88%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
@@ -1,14 +1,8 @@
+import numpy as np
 class Solution:
     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
-        dict_ = {}
-        dict_['col'] = [0]*n
-        dict_['row'] = [0]*m
-        for idx in range(len(indices)):
-            dict_['row'][indices[idx][0]] += 1
-            dict_['col'][indices[idx][1]] += 1
-        count_odd = 0
-        for row in range(m):
-            for col in range(n):
-                if (dict_['row'][row] + dict_['col'][col])&1 == 1:
-                    count_odd += 1
-        return count_odd
+        arr=np.zeros((m,n),dtype=int)
+        for row,col in indices:
+            arr[row,:]+=1
+            arr[:,col]+=1
+        return(len(arr[arr%2!=0]))
