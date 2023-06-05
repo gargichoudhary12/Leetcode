f15056e385fc24f7321f9b77d16e674018dab31d
gargichoudhary12 <104214078+gargichoudhary12@users.noreply.github.com>
2023-06-05 22:42:14 +0530

    Time: 45 ms (48.19%), Space: 16.4 MB (14.80%) - LeetHub

---
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.py
@@ -0,0 +1,8 @@
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        target=[]
+        j=0
+        for i in index:
+            target.insert(i,nums[j])
+            j+=1
+        return (target)
