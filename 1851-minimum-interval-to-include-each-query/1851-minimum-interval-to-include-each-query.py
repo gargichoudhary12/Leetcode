4151f97723862a66aa29cea56b8584fed2ce6eca
gargichoudhary12 <104214078+gargichoudhary12@users.noreply.github.com>
2023-06-21 23:04:29 +0530

    Time: 1711 ms (80.23%), Space: 58.3 MB (75.47%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.py
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.py
@@ -1,20 +1,15 @@
-from collections import deque
-import heapq
-
-
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        intervals = deque(sorted(intervals, key=lambda x: x[0]))
-        cache = {} # query -> min interval length
-        pq = []
-
-        for query in sorted(set(queries)):
-            while intervals and intervals[0][0] <= query:
-                l, r = intervals.popleft() # pop smallest start
-                heapq.heappush(pq, (r - l + 1, r)) # interval length, interval end
-
-            while pq and query > pq[0][1]: # intervals that are too small
-                heapq.heappop(pq)
-            cache[query] = pq[0][0] if pq else -1
-
-        return (cache[q] for q in queries)
+        intervals.sort()
+        minHeap = []
+        res = {}
+        i = 0
+        for q in sorted(queries):
+            while i < len(intervals) and intervals[i][0]<=q:
+                l,r = intervals[i]
+                heapq.heappush(minHeap,(r-l+1,r))
+                i+=1
+            while minHeap and minHeap[0][1]<q:
+                heapq.heappop(minHeap)
+            res[q] = minHeap[0][0] if minHeap else -1
+        return [res[q] for q in queries]
