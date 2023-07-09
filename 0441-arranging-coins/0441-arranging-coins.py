50a946259c90862c7e40983345c0845d4fb57403
gargichoudhary12 <104214078+gargichoudhary12@users.noreply.github.com>
2023-07-10 00:31:50 +0530

    Time: 47 ms (88.15%), Space: 16.4 MB (27.32%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,7 +1,8 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        left = 0
+        left = 1
         right = n
+        res = 0
         while left<=right:
             mid = (right+left)//2
             curr = mid*(mid+1)//2
@@ -11,5 +12,6 @@ class Solution:
                 right=mid-1
             else:
                 left = mid+1
-        return right
+                res = max(mid, res)
+        return res
         
