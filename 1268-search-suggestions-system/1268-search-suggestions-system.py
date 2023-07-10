dde6b6ec035aab68c69fb7ce178d50c29654e9e0
gargichoudhary12 <104214078+gargichoudhary12@users.noreply.github.com>
2023-07-11 00:51:54 +0530

    Time: 86 ms (90.19%), Space: 19.4 MB (91.89%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -0,0 +1,18 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        res = []
+        products.sort()
+        l =0
+        r = len(products)-1
+        for i in range(len(searchWord)):
+            c = searchWord[i]
+            while l<=r and (len(products[l])<=i or products[l][i]!=c):
+                l+=1
+                
+            while l<=r and (len(products[l])<=i or products[r][i]!=c):
+                r-=1
+            res.append([])
+            remain = r-l+1
+            for i in range(min(3, remain)):
+                res[-1].append(products[l+i])
+        return res
