f4474b6e973a0ac51d94e31e676e6cf123034530
gargichoudhary12 <104214078+gargichoudhary12@users.noreply.github.com>
2023-07-27 00:53:34 +0530

    Time: 345 ms (41.98%), Space: 157.3 MB (38.68%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -1,15 +1,15 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        dp = [0] * (high + 1)
-        dp[0] = 1
-        mod = 10 ** 9 + 7
-        for i in range(min(zero, one), high + 1):
-            if i >= zero:
-                dp[i] = (dp[i] + dp[i - zero]) % mod
-            if i >= one:
-                dp[i] = (dp[i] + dp[i - one]) % mod
-
-        s = 0
-        for i in range(low, high + 1):
-            s = (s + dp[i]) % mod
-        return s
+        mod = 10**9+7
+        dp = {}
+        
+        def helper(length):
+            if length>high:
+                return 0
+            if length in dp:
+                return dp[length]
+            dp[length] = 1 if length>=low else 0
+            dp[length]+= helper(length+zero)+helper(length+one)
+            
+            return dp[length]%mod
+        return helper(0)
