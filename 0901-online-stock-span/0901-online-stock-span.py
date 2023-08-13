a6da7ae77e6b922a86a0939f132ee1f74a387182
Gargi Choudhary <104214078+gargichoudhary12@users.noreply.github.com>
2023-08-13 11:51:58 +0530

    Time: 308 ms (98.70%), Space: 21.4 MB (95.94%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.py
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -7,7 +7,8 @@ class StockSpanner:
     def next(self, price: int) -> int:
         day =1
         while self.stack and self.stack[-1][0]<=price:
-            day+=self.stack.pop()[1]
+            day+=self.stack[-1][1]
+            self.stack.pop()
         self.stack.append((price,day))
         return day
 
