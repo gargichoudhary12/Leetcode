a155d9d4536161bd922ec67b9ab651a3b9262cdf
Gargi Choudhary <104214078+gargichoudhary12@users.noreply.github.com>
2023-08-29 15:43:15 +0530

    Time: 53 ms (86.30%), Space: 16.5 MB (63.12%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        str1 = Counter(ransomNote)
+        str2 = Counter(magazine)
+        if str1&str2 == str1:
+            return True
+        return False
