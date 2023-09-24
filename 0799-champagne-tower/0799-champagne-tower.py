ce0e3890c465a46beb448f83f08f570c541a05e5
Gargi Choudhary <104214078+gargichoudhary12@users.noreply.github.com>
2023-09-24 23:27:52 +0530

    Time: 78 ms (86.89%), Space: 16.2 MB (90.79%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -4,10 +4,10 @@ class Solution:
             return 0
         glasses = [poured]
         for i in range(2,query_row+2):
-            temp = [0]*i
+            ans = [0]*i
             for j in range(len(glasses)):
                 if (glasses[j]-1)/2 > 0:
-                    temp[j] += (glasses[j]-1)/2
-                    temp[j+1] += (glasses[j]-1)/2
-            glasses = temp
+                    ans[j] += (glasses[j]-1)/2
+                    ans[j+1] += (glasses[j]-1)/2
+            glasses = ans
         return min(1,glasses[query_glass])
